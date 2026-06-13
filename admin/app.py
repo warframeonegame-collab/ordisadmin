51a4f820ed5f7fb286d65287d2daaa8f25fa14be
warframeonegame-collab <warframeonegame@gmail.com>
2026-06-13 18:09:28 +0400

    Update port 5000 -> 3000 in app.py and .env

--- admin/app.py
+++ admin/app.py
@@ -896,4 +896,4 @@ def not_found(e):
 # ==================== MAIN ====================
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
